2293c389e1d78f88297cbc85d8214f7bc9c33c0a
kraleppa <misiek.nalepa@wp.pl>
2020-03-13 19:40:14 +0100

    ProcessManager in progress

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import subprocess
-from time import sleep
+from datetime import datetime
 import logging
+import time
 
 
-def get_process_name():
+def get_process_name() -> str:
     try:
         process = subprocess.Popen(["./script.sh"], stdout=subprocess.PIPE)
     except FileNotFoundError:
@@ -15,6 +16,29 @@ def get_process_name():
         return output
 
 
-for i in range(0, 100):
-    print(get_process_name())
-    sleep(0.5)
+def get_time() -> datetime.date:
+    return datetime.now()
+
+
+class ProcessManager:
+    def __init__(self):
+        self.current_process = None
+        self.current_process_begin = datetime.now()
+        self.process_list = list()
+
+    def update_process(self) -> bool:
+        current_process = get_process_name()
+        if self.current_process != current_process:
+            self.current_process = current_process
+            self.current_process_begin = datetime.now()
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    manager = ProcessManager()
+    for i in range(0, 40):
+        print(manager.current_process, datetime.now() - manager.current_process_begin)
+        time.sleep(1)
+        manager.update_process()
+
